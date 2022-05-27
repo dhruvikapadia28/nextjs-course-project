d9d9c4c4935b6177d5c4146a533ceed9a60b1895
dhruvi <dhruvikapadia28@gmail.com>
2022-05-27 11:42:49 +0530

    nextjs repo

---
+++ components/meetups/meetupList.js
@@ -0,0 +1,20 @@
+import MeetupItem from './meetupItem';
+import classes from './meetupList.module.css';
+
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default MeetupList;
